899ae9c2787951c43be2da3d4996798d8591bd3c
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-03-28 07:51:26 -0500

    set 1password to start minimized

--- modules/programs/1password.nix
+++ modules/programs/1password.nix
@@ -3,7 +3,19 @@
 {
   home-manager.users.${vars.user} = { config, ... }: {
     # Autostart
-    xdg.configFile."autostart/1password.desktop".source = config.lib.file.mkOutOfStoreSymlink "/run/current-system/sw/share/applications/1password.desktop";
+    #xdg.configFile."autostart/1password.desktop".source = config.lib.file.mkOutOfStoreSymlink "/run/current-system/sw/share/applications/1password.desktop";
+    xdg.configFile."autostart/1password.desktop".text = ''
+      [Desktop Entry]
+      Categories=Office;
+      Comment=Password manager and secure wallet
+      Exec=1password --silent %U
+      Icon=1password
+      MimeType=x-scheme-handler/onepassword;
+      Name=1Password
+      StartupWMClass=1Password
+      Terminal=false
+      Type=Application
+    '';
   };
 
   programs = {
