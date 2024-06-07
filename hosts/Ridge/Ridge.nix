7baebe6b3e38d46e06003f7d764681a8b6cf3678
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-06-07 12:31:02 -0500

    add logging to ridge undervolt service

--- hosts/Ridge/Ridge.nix
+++ hosts/Ridge/Ridge.nix
@@ -13,24 +13,35 @@ let
     # Find persistant device: readlink -f /sys/class/drm/card#/device
     GPU=/sys/devices/pci0000\:00/0000\:00\:03.1/0000\:08\:00.0/0000\:09\:00.0/0000\:0a\:00.0
 
-    # Set GPU max MHz - default max is 2664
-    echo s 1 2250 | sudo tee "$GPU"/pp_od_clk_voltage
-    # Set GPU voltage offset - default mV is 1200
-    echo vo -80 | sudo tee "$GPU"/pp_od_clk_voltage
-    # Set VRAM max MHz - default max is 1124
-    #echo m 1 1124 | sudo tee "$GPU"/pp_od_clk_voltage
+    # GPU max clock - default max is 2664
+    echo "Setting GPU clock"
+    echo s 1 2250 | tee "$GPU"/pp_od_clk_voltage
+    # Voltage offset - default mV is 1200
+    echo "Setting voltage offset"
+    echo vo -80 | tee "$GPU"/pp_od_clk_voltage
+    # VRAM max clock - default max is 1124 - not adjusting
+    #echo "Setting VRAM clock"
+    #echo m 1 1124 | tee "$GPU"/pp_od_clk_voltage
     # Apply values
-    echo c | sudo tee "$GPU"/pp_od_clk_voltage
+    echo "Applying undervolt settings"
+    echo c | tee "$GPU"/pp_od_clk_voltage
 
-    # Set max wattage (first 3 numbers are wattage) - default wattage is 284
-    echo 255000000 > "$GPU"/hwmon/hwmon2/power1_cap
+    # Power usage limit - default wattage is 284 (first 3 numbers are watts)
+    echo "Setting power usage limit"
+    echo 255000000 | tee "$GPU"/hwmon/hwmon2/power1_cap
 
-    # Set power profile level: auto, low, high, manual
-    echo manual > "$GPU"/power_dpm_force_performance_level
-    # Set power profile mode: cat "$GPU"/pp_power_profile_mode
-    echo 1 > "$GPU"/pp_power_profile_mode
-    # Set highest VRAM power state: cat "$GPU"/pp_dpm_mclk
-    echo 3 > "$GPU"/pp_dpm_mclk
+    # Performance level: auto, low, high, manual
+    echo "Setting performance level"
+    echo manual | tee "$GPU"/power_dpm_force_performance_level
+    # Power level mode: cat pp_power_profile_mode
+    echo "Setting power level mode"
+    echo 1 | tee "$GPU"/pp_power_profile_mode
+    # GPU power states: cat pp_dpm_sclk
+    #echo "Enabling all GPU power states"
+    #echo 1 | tee "$GPU"/pp_dpm_sclk
+    # VRAM power states: cat pp_dpm_mclk
+    echo "Enabling all VRAM power states"
+    echo 3 | tee "$GPU"/pp_dpm_mclk
   '';
 in {
   imports = lib.optional (builtins.pathExists ./swap.nix) ./swap.nix;
