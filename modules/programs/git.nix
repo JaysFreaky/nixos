4c6affcab44e49090812a50803ed4e54c5eb103e
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-06-09 14:36:36 -0500

    reorganized git credential methods

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -2,30 +2,35 @@
 let
   publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC/+CvZ9Cnq3Y4my0UtpH19dSNBJeT1wCPK7BAJyAvMA";
 in {
-  # Using credential.helper = "libsecret"; stores credentials inside gnome-keyring
-  # Relies upon gnome-keyring, libsecret, and seahorse setup in gnome.nix
+  # credential.helper = "libsecret" stores credentials inside gnome-keyring
+    # relies upon 'gnome-keyring', 'libsecret', and 'seahorse' pkgs in gnome.nix
   home-manager.users.${vars.user} = {
     programs.git = {
       enable = true;
       # gitFull contains git-credential-libsecret
       package = pkgs.gitFull;
+
+      extraConfig.init.defaultBranch = "main";
       userEmail = "95696624+JaysFreaky@users.noreply.github.com";
       userName = "JaysFreaky";
 
+      # libsecret
+      #extraConfig.credential.helper = "libsecret";
+
+      # SSH signing/commits
       extraConfig = {
         commit.gpgsign = true;
-        #credential.helper = "libsecret";
         gpg = {
           format = "ssh";
           ssh.program = "${pkgs._1password-gui}/bin/op-ssh-sign";
         };
-        init.defaultBranch = "main";
         user.signingkey = publicKey;
       };
     };
 
-    # Uses OAuth instead of libsecret - not as much setup/requirements
-    # Auto-injected into ~/.config/git
-    programs.git-credential-oauth.enable = true;
+    # Use either OAuth below or libsecret above (if not using SSH) - not both
+    # OAuth does not require additional pkgs like libsecret
+    #programs.git-credential-oauth.enable = true;
   };
+
 }
