18a35fab18db437f0c10cbb8cb57e159aed79f40
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-06-11 09:37:09 -0500

    add git & superfile bash alias

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -3,11 +3,6 @@
     programs.bash = {
       enable = true;
 
-      # This is causing issues with GNOME's night switcher extension:
-      # Probably fine on Hyprland
-      #if command -v wal > /dev/null 2>&1 && [ "$TERM" = "alacritty" ]; then
-          #wal -Rqe
-      #fi
       initExtra = ''
       '';
 
@@ -16,9 +11,12 @@
         ".df" = "cd /persist/etc/nixos";
         "ff" = "fastfetch";
         "fishies" = "asciiquarium";
+        "ga" = "git add";
+        "gc" = "git commit";
         "gd" = "git diff";
         "gs" = "git status";
         "ll" = "ls -la";
+        "spf" = "superfile";
       };
     };
   };
