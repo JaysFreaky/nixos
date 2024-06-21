08c0e7899f5d74a5d7659146228ed7e368520cfb
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-06-21 12:01:32 -0500

    set dynamic conditional git setups

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -1,36 +1,80 @@
-{ pkgs, vars, ... }:
+{ config, lib, pkgs, vars, ... }: with lib;
 let
   publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC/+CvZ9Cnq3Y4my0UtpH19dSNBJeT1wCPK7BAJyAvMA";
 in {
-  # credential.helper = "libsecret" stores credentials inside gnome-keyring
-    # relies upon 'gnome-keyring', 'libsecret', and 'seahorse' pkgs in gnome.nix
-  home-manager.users.${vars.user} = {
-    programs.git = {
-      enable = true;
-      # gitFull contains git-credential-libsecret
-      package = pkgs.gitFull;
+  options.git = {
+    libsecret.enable = mkOption {
+      default = false;
+      type = types.bool;
+    };
+    oauth.enable = mkOption {
+      default = false;
+      type = types.bool;
+    };
+    ssh.enable = mkOption {
+      default = true;
+      type = types.bool;
+    };
+  };
 
-      extraConfig.init.defaultBranch = "main";
-      userEmail = "95696624+JaysFreaky@users.noreply.github.com";
-      userName = "JaysFreaky";
+  config.home-manager.users.${vars.user} = mkMerge [
+    {
+      programs.git = {
+        enable = true;
+        extraConfig.init.defaultBranch = "main";
+        userEmail = "95696624+JaysFreaky@users.noreply.github.com";
+        userName = "JaysFreaky";
+      };
+    }
 
-      # libsecret
-      #extraConfig.credential.helper = "libsecret";
+    # credential.helper = "libsecret" stores credentials inside gnome-keyring
+      # relies upon 'gnome-keyring', 'libsecret', and 'seahorse' pkgs in ../desktops/gnome.nix
+    (mkIf (config.git.libsecret.enable) {
+      programs.git = {
+        extraConfig.credential.helper = "libsecret";
+        # gitFull contains git-credential-libsecret
+        package = pkgs.gitFull;
+      };
+    })
 
-      # SSH signing/commits
-      extraConfig = {
+    # OAuth does not require additional pkgs like libsecret
+    (mkIf (config.git.oauth.enable) {
+      programs.git-credential-oauth.enable = true;
+    })
+
+    # SSH signing/commits
+    (mkIf (config.git.ssh.enable) {
+      programs.git.extraConfig = {
         commit.gpgsign = true;
         gpg = {
           format = "ssh";
-          ssh.program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+          ssh = mkMerge [
+            (mkIf (config."1password".enable) {
+              program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+            })
+            (mkIf (!config."1password".enable) {
+              program = "${pkgs.openssh}/bin/ssh-agent";
+            })
+          ];
         };
         user.signingkey = publicKey;
       };
-    };
 
-    # Use either OAuth below or libsecret above (if not using SSH) - not both
-    # OAuth does not require additional pkgs like libsecret
-    #programs.git-credential-oauth.enable = true;
-  };
+      home.file.".ssh/config" = mkMerge [
+        (mkIf (config."1password".enable) {
+          text = ''
+            Host *
+              IdentityAgent ~/.1password/agent.sock
+          '';
+        })
+        (mkIf (!config."1password".enable) {
+          text = ''
+            Host github.com
+              ForwardAgent yes
+          '';
+        })
+      ];
+    })
+  ];
 
 }
