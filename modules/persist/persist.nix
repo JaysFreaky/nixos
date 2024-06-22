579897df645bde9b4ec3ddd27acaf3af2a242d7d
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-06-22 14:56:11 -0500

    ssh persistance keys were causing sshd to not start

--- modules/persist/persist.nix
+++ modules/persist/persist.nix
@@ -5,11 +5,6 @@
       "adjtime".source = "/persist/etc/adjtime";
       # Matching journalctl entries
       "machine-id".source = "/persist/etc/machine-id";
-      "nix/id_rsa".source = "/persist/etc/nix/id_rsa";
-      "ssh/ssh_host_rsa_key".source = "/persist/etc/ssh/ssh_host_rsa_key";
-      "ssh/ssh_host_rsa_key.pub".source = "/persist/etc/ssh/ssh_host_rsa_key.pub";
-      "ssh/ssh_host_ed25519_key".source = "/persist/etc/ssh/ssh_host_ed25519_key";
-      "ssh/ssh_host_ed25519_key.pub".source = "/persist/etc/ssh/ssh_host_ed25519_key.pub";
     };
 
     # Persistance module handles directories better than etc.<name>.source
