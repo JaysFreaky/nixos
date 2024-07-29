ecb0a073c8f47a7cb56bcb6d39977539fde69f4a
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-07-29 17:41:02 -0500

    removed some nvidia redundancies

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -6,39 +6,31 @@
 
   config = mkMerge [
     (mkIf (config.nvidia.enable) {
+      boot.kernelParams = [
+        # Nvidia - Framebuffer - https://wiki.archlinux.org/title/NVIDIA#DRM_kernel_mode_setting
+        "nvidia-drm.fbdev=1"
+      ];
+
       environment.systemPackages = with pkgs; [
-        # Monitoring
-          nvtopPackages.nvidia    # GPU stats
+        nvtopPackages.nvidia    # GPU stats
       ];
 
       hardware = {
-        graphics.extraPackages = with pkgs; [ nvidia-vaapi-driver ];
         nvidia = {
+          # kernelParams: "nvidia-drm.modeset=1"
           modesetting.enable = true;
           nvidiaSettings = true;
-          # Beta ships 555, which fixes Wayland issues - beta or stable
-            package = config.boot.kernelPackages.nvidiaPackages.beta;
-          #powerManagement = true;
+          # Current beta (555) fixes Wayland issues - beta or stable
+          package = config.boot.kernelPackages.nvidiaPackages.beta;
+          # kernelParams: "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+          powerManagement.enable = false;
         };
       };
 
-      services.xserver.videoDrivers = [ "nvidia" ];
-
-      boot = {
-        initrd.kernelModules = [
-          "nvidia"
-          "nvidia_drm"
-          "nvidia_modeset"
-          "nvidia_uvm"
-        ];
-        kernelParams = [
-          # Nvidia - Suspend
-            #"nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-          # Nvidia - Framebuffer
-            "nvidia_drm.fbdev=1"
-          # Nvidia - DKMS
-            "nvidia_drm.modeset=1"
-        ];
+      # Enabling these enables hardware.nvidia
+      services.xserver = {
+        enable = true;
+        videoDrivers = [ "nvidia" ];
       };
     })
 
@@ -47,13 +39,13 @@
         sessionVariables = {
           __GLX_VENDOR_LIBRARY_NAME = "nvidia";
           # GBM could possibily cause Firefox to crash - remove if so
-            GBM_BACKEND = "nvidia_drm";
+          GBM_BACKEND = "nvidia_drm";
 
           __GL_GSYNC_ALLOWED = 1;
           __GL_VRR_ALLOWED = 1;
 
           # Hardware Accelaration - 'nvidia' or 'vdpau'
-            LIBVA_DRIVER_NAME = "nvidia";
+          LIBVA_DRIVER_NAME = "nvidia";
           NVD_BACKEND = "direct";
         };
 
