a28513d6a72695bd09c87c54c12e135ef549c200
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-09-11 09:36:41 -0500

    Commenting out Gnome SSH agent

--- modules/desktops/gnome.nix
+++ modules/desktops/gnome.nix
@@ -425,10 +425,10 @@ in {
         kitty.extraConfig = ''include /home/${vars.user}/.config/kitty/current-theme.conf'';
 
         # SSH agent
-        bash.initExtra = ''
+        /*bash.initExtra = ''
           eval $(/run/wrappers/bin/gnome-keyring-daemon --start --daemonize)
           export SSH_AUTH_SOCK
-        '';
+        '';*/
       };
 
       # Set Nautilus bookmarks
