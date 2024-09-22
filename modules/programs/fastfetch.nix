c59cf6b1fa9a7e54edaaa8ecbb1b254cb9825f00
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-09-21 19:13:04 -0500

    Updated logo for any terminal and removed duplicate CPU GHz

--- modules/programs/fastfetch.nix
+++ modules/programs/fastfetch.nix
@@ -5,9 +5,9 @@
       settings = {
         "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
         logo = {
-          type = "kitty-direct";
-          source = ../../assets/logo.png;
-          #source = "NixOS_small";
+          #type = "kitty-direct";
+          #source = ../../assets/logo.png;
+          source = "NixOS_small";
           width = 20;
           padding = {
             left = 6;
@@ -57,7 +57,7 @@
           {
             type = "cpu";
             key = "";
-            format = "{1} ({5}) @ {7} GHz";
+            format = "{1} ({5}) @ {7}";
           }
           {
             type = "gpu";
