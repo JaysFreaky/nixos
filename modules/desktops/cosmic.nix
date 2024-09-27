12e09b09207d00abf3bcc18cfe26e9663446d1f2
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2024-09-26 21:02:52 -0500

    Moved Cosmic's cachix to common so it caches prior to switching to Cosmic DE

--- modules/desktops/cosmic.nix
+++ modules/desktops/cosmic.nix
@@ -34,11 +34,6 @@ in {
       neovide                     # GUI launcher for neovim
     ];
 
-    nix.settings = {
-      substituters = [ "https://cosmic.cachix.org/" ];
-      trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-    };
-
     services = {
       desktopManager.cosmic.enable = true;
       displayManager.cosmic-greeter.enable = true;
