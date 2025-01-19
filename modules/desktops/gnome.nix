3f8b407f7e66654ff7ef9ceaffe01a0b33dacdad
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2025-01-19 17:09:06 -0600

    Unset Gnome app launching so window shortcuts work correctly

--- modules/desktops/gnome.nix
+++ modules/desktops/gnome.nix
@@ -406,6 +406,12 @@ in {
           switch-to-application-2 = [];
           switch-to-application-3 = [];
           switch-to-application-4 = [];
+          switch-to-application-5 = [];
+          switch-to-application-6 = [];
+          switch-to-application-7 = [];
+          switch-to-application-8 = [];
+          switch-to-application-9 = [];
+          switch-to-application-0 = [];
           toggle-message-tray = [];
         };
         "org/gnome/shell/weather".automatic-location = true;
