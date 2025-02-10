bfd10a3bed0367e6acf9e41828a66034029ee980
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2025-02-09 22:10:04 -0600

    Add/Update bash aliases (exiftool)

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -10,15 +10,18 @@
       shellAliases = {
         ".." = "cd ..";
         ".df" = "cd ${nixPath}";
+        "bonsai" = "cbonsai --screensaver";
+        "exifinfo" = "exiftool -a -g -s \"$@\"";
+        "exifstrip" = "exiftool -All= \"$@\"";
         "ff" = "fastfetch";
-        "fishies" = "asciiquarium";
+        "fishy" = "asciiquarium";
         "ga" = "git add";
         "gc" = "git commit";
         "gd" = "git diff";
         "gs" = "git status";
         "ll" = "ls -la";
         "nixdiff" = "nixos-rebuild build && nix store diff-closures /run/current-system ./result";
-        "ns" = "nix search nixpkgs#\"$@\"";
+        "ns" = "nix search nixpkgs#\"$1\"";
         "spf" = "superfile";
       };
     };
