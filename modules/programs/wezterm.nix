9291d1b22123e5d3e25e4704a8a0be07668e7128
JaysFreaky <95696624+JaysFreaky@users.noreply.github.com>
2025-03-15 10:31:45 -0500

    wezterm: adjust styling

--- modules/programs/wezterm.nix
+++ modules/programs/wezterm.nix
@@ -1,58 +1,64 @@
 {
+  config,
+  lib,
+  pkgs,
   cfgOpts,
   inputs,
-  lib,
   myUser,
-  pkgs,
   ...
 }: let
   cfg = cfgOpts.wezterm;
+  stylix = config.stylix.enable;
+
+  dark = "Catppuccin Mocha";
+  light = "Catppuccin Frappe";
 in {
   options.myOptions.wezterm.enable = lib.mkEnableOption "Wezterm";
 
   config = lib.mkIf (cfg.enable) {
-    home-manager.users.${myUser} = {
-      programs.wezterm = {
-        enable = true;
-        enableBashIntegration = false;
-        enableZshIntegration = false;
-        package = inputs.wezterm.packages.${pkgs.system}.default;
-        extraConfig = ''
-          function get_appearance()
-            if wezterm.gui then
-              return wezterm.gui.get_appearance()
-            end
-            return 'Dark'
+    home-manager.users.${myUser}.programs.wezterm = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = false;
+      package = inputs.wezterm.packages.${pkgs.system}.default;
+      extraConfig = ''
+        local xcursor_size = nil
+        local xcursor_theme = nil
+
+        local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"})
+        if success then
+          xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
+        end
+
+        local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-size"})
+        if success then
+          xcursor_size = tonumber(stdout)
+        end
+
+        return {
+          xcursor_theme = xcursor_theme,
+          xcursor_size = xcursor_size,
+        }
+      '' ++ lib.optional (!stylix) ''
+        function get_appearance()
+          if wezterm.gui then
+            return wezterm.gui.get_appearance()
           end
+          return 'Dark'
+        end
 
-          function scheme_for_appearance(appearance)
-            if appearance:find 'Dark' then
-              return 'tokyonight-storm'
-            else
-              return 'tokyonight-day'
-            end
+        function scheme_for_appearance(appearance)
+          if appearance:find 'Dark' then
+            return '${dark}'
+          else
+            return '${light}'
           end
+        end
 
-          local xcursor_size = nil
-          local xcursor_theme = nil
-
-          local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"})
-          if success then
-            xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
-          end
-
-          local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-size"})
-          if success then
-            xcursor_size = tonumber(stdout)
-          end
-
-          return {
-            color_scheme = scheme_for_appearance(get_appearance()),
-            xcursor_theme = xcursor_theme,
-            xcursor_size = xcursor_size,
-          }
-        '';
-      };
+        return {
+          color_scheme = scheme_for_appearance(get_appearance()),
+        }
+      '';
     };
   };
 }
